d5a18ac61c3a043b110b5885ac660d264a8145a5
Erick Cardoso <Usurio-PC@DESKTOP-1LR3709>
2023-05-01 20:17:02 -0300

    integracao api consulta crm

---
+++ ApiMobile/Controllers/CRMController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using ApiMobile.Services;
+using Microsoft.AspNetCore.Mvc;
+using ApiMobile.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiMobile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CRMController : ControllerBase
+    {
+        private readonly ICRMApiService _crmApiService;
+
+        public CRMController(ICRMApiService crmApiService)
+        {
+            _crmApiService = crmApiService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ConsultaCRMResult>> GetCRM(string numero, string uf)
+        {
+            
+            var crms = await _crmApiService.GetMedicos(numero, uf);
+
+            return crms;
+        }
+    }
+}
---
+++ ApiMobile/Models/ConsultaCRMResult.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json.Serialization;
+
+namespace ApiMobile.Models
+{
+    public class ConsultaCRMResult
+    {
+        [JsonPropertyName("url")]
+        public string URL { get; set; }
+        [JsonPropertyName("total")]
+        public int Total { get; set; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+        [JsonPropertyName("mensagem")]
+        public string Mensagem { get; set; }
+        [JsonPropertyName("api_limite")]
+        public string ApiLimite { get; set; }
+        [JsonPropertyName("api_consultas")]
+        public string ApiConsultas { get; set; }
+        [JsonPropertyName("item")]
+        public ICollection<ConsultaCRM> ConsultaCRM {get; set;}
+    }
+}
