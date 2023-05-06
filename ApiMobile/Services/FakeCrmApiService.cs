c9a28e9c55c6708ca10304b29aa144adf5e9d3e1
dvdgd <dvd.gd762@gmail.com>
2023-05-06 15:06:31 -0300

    Adicionando FakeCRMApiService

---
+++ ApiMobile/Services/FakeCrmApiService.cs
@@ -0,0 +1,34 @@
+﻿using ApiMobile.DTO;
+using ApiMobile.Services.Interfaces;
+
+namespace ApiMobile.Services
+{
+    public class FakeCrmApiService : ICRMApiService
+    {
+        public Task<ConsultaCRMResult> GetMedicosAsync(string numero, string uf)
+        {
+            var consultaCRMResult = new ConsultaCRMResult
+            {
+                URL = "https://example.com",
+                Total = 1,
+                Status = "Success",
+                Mensagem = "Consulta CRM result",
+                ApiLimite = "10",
+                ApiConsultas = "https://api.example.com",
+                Itens = new List<ConsultaCRM>
+                {
+                    new ConsultaCRM
+                    {
+                        Nome = "teste",
+                        Numero = numero,
+                        Profissao = "Testador",
+                        Situacao = "ativo",
+                        Tipo = "CRM",
+                        UF = uf
+                    },
+                }
+            };
+            return Task.FromResult(consultaCRMResult);
+        }
+    }
+}
