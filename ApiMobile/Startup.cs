37533211537121cdaef3fe8cfd38bc7ab8135942
dvdgd <dvd.gd762@gmail.com>
2023-05-17 15:37:01 -0300

    Removendo rotas Post Put e Delete de DiasSemana

--- ApiMobile/Startup.cs
+++ ApiMobile/Startup.cs
@@ -26,7 +26,7 @@ namespace ApiMobile
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Prev Ler", Version = "v1" });
 
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                 {
