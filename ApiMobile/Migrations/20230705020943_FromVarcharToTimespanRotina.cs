8d3041e8c26e468616561e7fa74441e5d8ef2d7f
dvdgd <dvd.gd762@gmail.com>
2023-07-04 23:13:27 -0300

    Coluna invervalo alterada para timespan em rotina

---
+++ ApiMobile/Migrations/20230705020943_FromVarcharToTimespanRotina.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiMobile.Migrations
+{
+    /// <inheritdoc />
+    public partial class FromVarcharToTimespanRotina : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE Rotina
+                ALTER COLUMN Intervalo TIME(0)
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE Rotina
+                ALTER COLUMN Intervalo VARCHAR(MAX)
+            ");
+        }
+    }
+}
